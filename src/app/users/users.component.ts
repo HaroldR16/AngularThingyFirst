ac42bf5a0f2cb213b29bb6f505a78499755a91e4
HaroldR16 <josu.rea@outlook.com>
2021-05-23 15:39:22 -0600

    FormGroupcommit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,9 +8,21 @@ import {FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public formBuilder: FormBuilder
+    ) { }
+    userForm!: FormGroup;
+    
+    ngOnInit(): void {
+      this.userForm = this.formBuilder.group({
+        username: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+        fullName: new FormControl('',[Validators.required, Validators.minLength(1), Validators.maxLength(100)]),
+        email: new FormControl('',[Validators.required, Validators.minLength(5)]),
+        phone: new FormControl('')
+      });
   }
 
+  get getControl(){
+    return this.userForm.controls;
+  }
 }
